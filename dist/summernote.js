4834919ea2bd602da8d30077997180279f7324fb
Tim Canty <timmcanty@gmail.com>
2015-03-25 17:20:12 -0400

    Changes definition of empty para for email

--- dist/summernote.js
+++ dist/summernote.js
@@ -1366,7 +1366,7 @@
       /** @property {String} blank */
       blank: blankHTML,
       /** @property {String} emptyPara */
-      emptyPara: '<p>' + blankHTML + '</p>',
+      emptyPara: '<br><br>',
       makePredByNodeName: makePredByNodeName,
       isEditable: isEditable,
       isControlSizing: isControlSizing,
